5f4777553ca15f8229b63e7e7838271b5e135155
HalcyonJX <502229027@qq.com>
2024-10-26 19:47:43 +0800

    Initial commit

---
+++ src/test/java/com/yupi/usercenter/service/UserServiceTest.java
@@ -0,0 +1,34 @@
+package com.yupi.usercenter.service;
+
+// [编程学习交流圈](https://www.code-nav.cn/) 连接万名编程爱好者，一起优秀！20000+ 小伙伴交流分享、40+ 大厂嘉宾一对一答疑、100+ 各方向编程交流群、4000+ 编程问答参考
+
+import com.yupi.usercenter.model.domain.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 用户服务测试
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+public class UserServiceTest {
+
+    @Resource
+    private UserService userService;
+
+
+    @Test
+    void searchUserByTags() {
+        List<String> tagNameList = Arrays.asList("Java", "Python");
+        List<User> userList = userService.searchUserByTags(tagNameList);
+        Assert.assertNotNull(userList);
+    }
+}
